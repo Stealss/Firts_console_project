ed21be7c2854de9590b59f9b414447251c0736a1
Rostislav Kuzmenko <Ros.Kuzmenko@gmail.com>
2023-01-14 12:13:19 +0200

    done

--- study.js
+++ study.js
@@ -22,3 +22,18 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const lastMovie1 = prompt('Один из последних просмотренных фильмов?','');
+const raiting1 = prompt('На сколько оцените его?','');
+const lastMovie2 = prompt('Один из последних просмотренных фильмов?','');
+const raiting2 = prompt('На сколько оцените его?','');
+personalMovieDB.movies[lastMovie1] = raiting1;
+personalMovieDB.movies[lastMovie2] = raiting2;
+console.log(personalMovieDB);
